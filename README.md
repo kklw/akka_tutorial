21fba55a5f442f9a064ae10f0793076eb7060c7d
Kavan Koh <kavanohk@gmail.com>
2018-06-21 17:51:03 +0800

    Part 2 & 3 of tutorial. Created Supervisor and Device actor.

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Right click pom.xml add as maven project
 
 # Notes
 ## Part 1 : Actor hierarchy in action
+Package: actor_architecture
 
 Actor Hierarchy
 - ActorHierarchyExperiments.java
@@ -15,3 +16,20 @@ Actor Lifecycle
 
 Failure Handling
 - SupervisingActorMain.java
+
+## Part 2 : Creating the first actor
+Package: iot
+
+Files:
+- iotMain.java
+- iotSupervisor.java
+
+IotSupervisor represents the whole application.
+
+## Part 3 : Working with Device Actors
+Define device actor
+
+Read & write messages
+
+Testing the actor
+
---
+++ src/main/java/iot/Device.java
@@ -0,0 +1,91 @@
+package iot;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.Optional;
+
+class Device extends AbstractActor {
+    private final LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    final String groupId;
+
+    final String deviceId;
+
+    public Device(String groupId, String deviceId) {
+        this.groupId = groupId;
+        this.deviceId = deviceId;
+    }
+
+    public static Props props(String groupId, String deviceId) {
+        return Props.create(Device.class, groupId, deviceId);
+    }
+
+    public static final class RecordTemperature {
+        final long requestId;
+        final double value;
+
+        public RecordTemperature(long requestId, double value) {
+            this.requestId = requestId;
+            this.value = value;
+        }
+    }
+
+    public static final class TemperatureRecorded {
+        final long requestId;
+
+        public TemperatureRecorded(long requestId) {
+            this.requestId = requestId;
+        }
+    }
+
+    public static final class ReadTemperature {
+        long requestId;
+
+        public ReadTemperature(long requestId) {
+            this.requestId = requestId;
+        }
+    }
+
+    public static final class RespondTemperature {
+        long requestId;
+        Optional<Double> value;
+
+        public RespondTemperature(long requestId, Optional<Double> value) {
+            this.requestId = requestId;
+            this.value = value;
+        }
+    }
+
+    Optional<Double> lastTemperatureReading = Optional.empty();
+
+    @Override
+    public void preStart() {
+        log.info("Device actor {}-{} started", groupId, deviceId);
+    }
+
+    @Override
+    public void postStop() {
+        log.info("Device actor {}-{} stopped", groupId, deviceId);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                // Write temperature
+                // Informs sender that write is successful
+                .match(RecordTemperature.class, r -> {
+                    log.info("Recorded temperature reading {} with {}", r.value, r.requestId);
+                    lastTemperatureReading = Optional.of(r.value);
+                    getSender().tell(new TemperatureRecorded(r.requestId), getSelf());
+                })
+                // Read temperature
+                .match(ReadTemperature.class, r -> {
+                    getSender().tell(new RespondTemperature(r.requestId, lastTemperatureReading), getSelf());
+                })
+                .build();
+    }
+
+}
